4e389bdc8007bee14a3b876df221ae9ede998c21
richardfigueroa <richard.figueroa@live.com>
2022-03-15 01:09:06 -0400

    Implement basic GUI for billboard component

--- front-end/src/views/create/Billboard.vue
+++ front-end/src/views/create/Billboard.vue
@@ -1,10 +1,105 @@
 <template>
-  <div>
-    <h1>billboard</h1>
-    <p>{{ billboard }}</p>
-    <h1>billboards</h1>
-    <p>{{ billboards }}</p>
-  </div>
+    <v-container fluid class="fluid-background">
+   <v-row>
+      <v-col sm="12">
+        <h2
+          class="d-flex justify-center" style = "border: solid #CCCCCC">
+          Billboard Pozter Selector
+          </h2>
+        <v-row no-gutters>
+          <v-col
+            cols="8"
+            sm="2"
+          >
+      <v-card>
+        <v-navigation-drawer permanent>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Application
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                subtext
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list
+            dense
+            nav
+             v-for="option in sidebarOptions"
+             :key="option.title"
+          >
+          <!-- Below is where you would provide the items for the list -->
+            <v-list-item link>
+                <v-list-item-icon>
+                  <v-icon>{{option.icon}}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title >{{option.title}}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+      </v-card>
+          </v-col>
+          <!-- the 2 x 2 in the center -->
+            <v-col
+              cols="4"
+              sm="10"
+            >
+      <v-row >
+        <v-col
+          v-for="card in cards"
+          :key="card.title"
+          :cols="card.flex"
+        >
+          <v-card>
+          <!-- image -->
+            <v-img
+              :src="card.src"
+              contain
+              height="200px"
+            >
+              
+            </v-img>
+              <v-card-title class="justify-center" v-text="card.title"></v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+      
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <!-- button section -->
+        <v-row class="mb-6">
+        <v-col 
+            md="1"
+            offset-md="8"
+            >
+            <div>  
+            <v-btn>Cancel</v-btn>
+            </div>
+            </v-col>
+               <v-col 
+                md="1"
+                offset-md="1"
+            >
+            <div>  
+               <v-btn color="orange">
+              Next
+              </v-btn>
+            </div>
+          
+            </v-col>
+            </v-row>
+      </v-row>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    </v-container>
+
 </template>
 
 <script>
@@ -17,6 +112,29 @@ export default {
     return {
       billboard: {},
       billboards: [],
+      sidebarOptions: [
+        {title: 'My Files', icon: 'mdi-folder'},
+        {title: 'Shared with me', icon: 'mdi-account-multiple'},
+        {title: 'Starred', icon: 'mdi-star'},
+        {title: 'My Files', icon: 'mdi-folder'},
+        {title: 'Shared with me', icon: 'mdi-account-multiple'},
+        {title: 'Starred', icon: 'mdi-star'},
+        {title: 'My Files', icon: 'mdi-folder'},
+        {title: 'Shared with me', icon: 'mdi-account-multiple'},
+        {title: 'Starred', icon: 'mdi-star'},
+        {title: 'My Files', icon: 'mdi-folder'},
+        {title: 'Shared with me', icon: 'mdi-account-multiple'},
+        {title: 'Starred', icon: 'mdi-star'},
+        {title: 'My Files', icon: 'mdi-folder'},
+        {title: 'Shared with me', icon: 'mdi-account-multiple'},
+        {title: 'Starred', icon: 'mdi-star'},
+      ],
+      cards: [
+        { title: 'CF Bay-Dundas Digital', src: require('../../assets/canvas_1.jpg'), flex: 6 },
+        { title: 'CFTEC Tower Digital', src: require('../../assets/canvas_2.jpg'), flex: 6 },
+        { title: 'AOBOne Digital Screen', src: require('../../assets/canvas_1.jpg'), flex: 6 },
+        { title: 'AOB Two Digital Screen', src: require('../../assets/canvas_1.jpg'), flex: 6 },
+      ],
     };
   },
   async mounted() {
@@ -36,4 +154,8 @@ export default {
 </script>
 
 <style scoped>
+
 </style>
+
+
+
