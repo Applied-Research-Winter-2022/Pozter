c18f41425889de7d26887ae49d40bbf442d17021
Jess Nguyen <nguyjess@gmail.com>
2022-03-18 00:35:05 -0400

    Home button with routes going back and forth in to create

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -14,6 +14,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    alias: "/home",
     component: Home,
   },
   {
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>Home page</div>
+  <div>
+    <v-main>
+      <v-container fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs6>
+            <div class="text-xs-center">
+              <router-link to="/create" tag="button">
+                <v-btn  class="mx-auto" id="button-create" color="orange">
+                  Create New
+                </v-btn>
+              </router-link>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </div>
 </template>
 
 <script>
+
 export default {
   name: "Home",
   props: {},
   data() {
     return {};
+  },
+  components:{
   },
   computed: {},
   methods: {},
