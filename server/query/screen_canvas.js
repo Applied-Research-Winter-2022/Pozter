ac1feee8ffdd3077c1120e8c4d7eedc09857c322
Joseph Haley <haley.joseph@gmail.com>
2022-03-24 15:58:54 -0400

    Copied Jess' validation code and applied it to other endpoints

--- server/query/screen_canvas.js
+++ server/query/screen_canvas.js
@@ -1,9 +1,20 @@
 const db = require("../connection/pg");
+const Joi = require("joi");
+
+const idSchema = Joi.object().keys({
+  id: Joi.string().guid().required(),
+});
 
 module.exports = {
   async fetchById(id) {
-    const screenCanvas = await db.query(`SELECT * FROM screen_canvases WHERE id = '${id}'`);
-    return screenCanvas;
+    const validation = idSchema.validate({ id });
+    if (validation.error) {
+      console.error(validation.error);
+      return validation.error;
+    } else {
+      const screenCanvas = await db.query(`SELECT * FROM screen_canvases WHERE id = '${id}'`);
+      return screenCanvas.rows;
+    }
   },
 
   async fetchAll() {
--- server/query/social_media_post.js
+++ server/query/social_media_post.js
@@ -1,15 +1,41 @@
 const db = require("../connection/pg");
+const Joi = require("joi");
+
+const createSchema = Joi.object().keys({
+  social_media_content_id: Joi.string().guid().required(),
+  content: Joi.string(),
+});
+
+const idSchema = Joi.object().keys({
+  id: Joi.string().guid().required(),
+});
 
 module.exports = {
   async create({ social_media_content_id, content }) {
-    let query = "INSERT INTO social_media_posts (social_media_content_id, content) ";
-    query += `VALUES ('${social_media_content_id}', '${content}')`;
-    return db.query(query);
+    const validation = createSchema.validate({ social_media_content_id, content });
+    if (validation.error) {
+      console.error(validation.error);
+      return validation.error;
+    } else {
+      const result = await db
+        .query(
+          `INSERT INTO social_media_posts (social_media_content_id, content) VALUES ('${social_media_content_id}', '${content}') returning id`
+        )
+        .then((res) => res.rows)
+        .catch((err) => Promise.reject(err));
+      return result;
+    }
   },
 
   async fetchById(id) {
-    const socialMediaPost = await db.query(`SELECT * FROM social_media_posts WHERE id = '${id}'`);
-    return socialMediaPost;
+    const validation = idSchema.validate({ id });
+    if (validation.error) {
+      console.error(validation.error);
+      return validation.error;
+    } else {
+      const socialMediaPost = await db.query(`SELECT * FROM social_media_posts WHERE id = '${id}'`);
+      return socialMediaPost;
+    }
   },
 
   async fetchAll() {
