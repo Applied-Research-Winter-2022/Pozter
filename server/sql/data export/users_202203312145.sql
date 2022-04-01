03adef6fae4fe8f5be0b87620b3f5eb2d59b0b76
Jess Nguyen <nguyjess@gmail.com>
2022-03-31 22:36:00 -0400

    Rebuild db for demo

---
+++ server/sql/data export/users_202203312145.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (id,user_name,organization,created_at,updated_at) VALUES
+	 ('35a27f4e-9f40-11ec-bbaa-bba14276ac27'::uuid,'User 1','Organization 1','2022-03-08 19:31:06.819953-05',NULL);
